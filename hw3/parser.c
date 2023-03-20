1006bebd5cc529983417dec7fdf92882c53c67f6
Robert Perez <rickymcmuffins@gmail.com>
2023-03-20 11:25:02 -0400

    finished var and const

--- hw3/parser.c
+++ hw3/parser.c
@@ -9,98 +9,141 @@ token *ret;
 
 AST *parser_open(char *fileName)
 {
-    fp = fopen(fileName, "r");
+	fp = fopen(fileName, "r");
 
-    if (fp == NULL)
-    {
-        bail_with_error("ERROR: woah there PAL, you put in an invalid file name! Try again with one that works :)\n");
-        return 0;       
-    }
+	if (fp == NULL)
+	{
+		bail_with_error("ERROR: woah there PAL, you put in an invalid file name! Try again with one that works :)\n");
+		return 0;
+	}
 
-    lexer_open(fileName);
-    
-    return parseProgram();
+	lexer_open(fileName);
+
+	return parseProgram();
 }
 
 token eat(token_type tokenName)
 {
-	if (ret->typ == tokenName) 
-        *ret = lexer_next();
+	token *old = ret;
+	if (ret->typ == tokenName)
+		*ret = lexer_next();
 
-    return *ret;
+	return *old;
 }
 
-AST* parseProgram()
+AST *parseProgram()
 {
-	AST* prog;
-    	AST_list cds = parseConstDecls();
-    	AST_list vds = parseVarDecls();
-    	AST* stmt = parseStmt();
-	
-	prog = ast_program(ret->filename, lexer_line(), lexer_column(), cds, vds, stmt); 
+	AST *prog;
+	AST_list cds = parseConstDecls();
+	AST_list vds = parseVarDecls();
+	AST *stmt = parseStmt();
+
+	prog = ast_program(ret->filename, lexer_line(), lexer_column(), cds, vds, stmt);
 	eat(periodsym);
 	return prog;
 }
 
 AST_list parseConstDecls()
 {
-    AST_list cds = malloc(100 * sizeof(AST_list));
-    int i = 0;
-    
-    while (1)
-    {
-        if (ret->typ != constsym)
-            return cds;
-        
-        cds[i] = parseConstDecl();
-        i++;
-    }
+	AST_list cds = ast_list_empty_list();
+
+
+	while (1)
+	{
+		if (ret->typ != constsym)
+			return cds;
+
+		ast_list_splice(cds, parseConstDeclLine());
+		
+	}
+} 
+
+AST_list parseConstDeclLine(){
+
+	AST_list cds;
+
+	eat(constsym);
+
+	cds = ast_list_singleton(parseConstDecl());
+	
+	while(1){
+		if(ret->typ = semisym){
+			eat(semisym);
+			return cds;
+		}
+		eat(commasym);
+		ast_list_splice(cds, parseConstDecl());
+	}
+
 }
 
-AST * parseConstDecl(){
-	AST * cd;
-	token cons = eat(constsym);
+AST *parseConstDecl()
+{
+	AST *cd;
 	token iden = eat(identsym);
 	eat(eqsym);
 	token num = eat(numbersym);
-	
-	cd = ast_const_def(cons, iden.text, num.value);
+
+	cd = ast_const_def(iden, iden.text, num.value);
 	return cd;
 }
 
-
-AST_list parseVarDelcs()
+AST_list parseVarDecls()
 {
-	AST_list vds = malloc(100 * sizeof(AST_list));
-    int i = 0;
-    
-    while (1)
-    {
-        if (ret->typ != varsym)
-            return vds;
-        
-        vds[i] = parseVarDecl();
-        i++;
-    }
+	AST_list vds = ast_list_empty_list();
+
+
+	while (1)
+	{
+		if (ret->typ != varsym)
+			return vds;
+
+		ast_list_splice(vds, parseConstDeclLine());
+		
+	}
+} 
+
+AST_list parseVarDeclLine(){
+
+	AST_list vds;
+
+	eat(varsym);
+
+	vds = ast_list_singleton(parseVarDecl());
+	
+	while(1){
+		if(ret->typ = semisym){
+			eat(semisym);
+			return vds;
+		}
+		eat(commasym);
+		ast_list_splice(vds, parseVarDecl());
+	}
+
 }
 
-AST *parseVarDelc()
+AST *parseVarDecl()
 {
-    eat(varsym);
-    eat(identsym);
-    return ast_var_decl(*ret, ret->text);
+	AST *vd;
+	token iden = eat(identsym);
+
+	vd = ast_var_decl(iden, iden.text);
+	return vd;
 }
 
-void parseCommmaIdents(){
+void parseCommmaIdents()
+{
 	eat(commasym);
 	eat(identsym);
 }
 // not done
-AST *parseStmt(){
-	switch(ret->typ){
-		
-		default:
-			break;
+AST *parseStmt()
+{
+	switch (ret->typ)
+	{
+
+	default:
+		break;
 	}
 }
 
@@ -116,7 +159,7 @@ void parseExpr();
 
 void parseAddSubTerm();
 
-void parseAddSub(); 
+void parseAddSub();
 
 void parseTerm();
 
@@ -126,31 +169,28 @@ void parseMultDiv();
 
 void parseFactor()
 {
-    switch(ret->typ)
-    {
-        case identsym:
-            eat(identsym);
-            break;
-    }
-   
-    
+	switch (ret->typ)
+	{
+	case identsym:
+		eat(identsym);
+		break;
+	}
 }
 
-AST* parseSign()
+AST *parseSign()
 {
-    switch(ret->typ)
-    {
-        case plussym:
-            eat(plussym);
-            break;
-        case minussym:
-            eat(minussym);
-            break;
-        default:
-            token_type expected[] = {plussym, minussym};
-            parse_error_unexpected(expected, 2, *ret); 
-    }
-    AST* expr = parseExpr();
-    return ast_op_expr(*ret, )
+	switch (ret->typ)
+	{
+	case plussym:
+		eat(plussym);
+		break;
+	case minussym:
+		eat(minussym);
+		break;
+	default:
+		token_type expected[] = {plussym, minussym};
+		parse_error_unexpected(expected, 2, *ret);
+	}
+	AST *expr = parseExpr();
+	return ast_op_expr(*ret, )
 }
-
